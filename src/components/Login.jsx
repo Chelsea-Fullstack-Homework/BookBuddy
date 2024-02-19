7c9266e1dbbd79835f52bac8a09d6ba49d6dbad9
Chelsea Scott <chelseacat62@gmail.com>
2024-02-18 17:58:03 -0700

    render component based on token condition

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,16 @@
 /* TODO - add your code to create a functional React component that renders a login form */
+import LoginForm from './LoginForm'
 
-function Login(){
+function Login({ user, setUser, token, setToken }){
     return (
         <>
-            <h1>Log in here</h1>
+        {
+            (token) 
+            ?(<h1>Logged in as {user}</h1>)
+            :(<LoginForm 
+                setUser={setUser} 
+                setToken={setToken} />)
+        }
         </>
     )
 }
