27c78ade099bdd90e4de6f53014933c7c6237ac7
Chelsea Scott <chelseacat62@gmail.com>
2024-02-18 21:23:27 -0700

    rendered a button to link to single book detail page

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,11 +1,17 @@
-function Book({book}){
+import { Link } from 'react-router-dom'
+
+function Book({book, setSelectedBook}){
 
     return (
         <>
             <div className='book'>
                 <h3>{book.title}</h3>
-                <img src={book.coverimage}></img>
-                <button onClick={()=>{}}><h3>Details</h3></button>
+                <div className='book-details'>
+                    <img src={book.coverimage}></img>
+                    <Link to={`/books/${book.id}`}>
+                        <button><h3>Details</h3></button>
+                    </Link>
+                </div>
             </div>
         </>
     )
