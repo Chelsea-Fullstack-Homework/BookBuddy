8ca01cb82cd232a887ec91fc4878ae00d5a7faab
Chelsea Scott <chelseacat62@gmail.com>
2024-02-20 01:49:45 -0700

    render single book details from url params

--- src/components/SingleBookDetails.jsx
+++ src/components/SingleBookDetails.jsx
@@ -1,4 +1,25 @@
-function SingleBookDetails({bookDetails}){
+function SingleBookDetails({bookDetails, token}){
+
+    const handleClick = async()=>{
+        try{
+            const endpoint = `api/books/${bookDetails.id}`;
+            const url = `https://fsa-book-buddy-b6e748d1380d.herokuapp.com/${endpoint}`;
+            await fetch(url, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify({
+                    available: 'false',
+                }),
+            });
+        }catch(err){
+            console.error(err);
+        }
+    }
+
+
     return (
         <>
             <ul>
@@ -11,11 +32,15 @@ function SingleBookDetails({bookDetails}){
                 {
                     bookDetails.available?
                     (
-                        <>
-                        <li>This book is currently available for checkout</li>
-                        <button><h3>Checkout</h3></button>
-                        </>
-                    ):
+                        token? 
+                        (
+                            <>
+                            <li>This book is currently available for checkout</li>
+                            <button onClick={()=>{handleClick()}}><h3>Checkout</h3></button>
+                            </>
+                        ): // user does not present a valid token
+                        <li>This book is currently available for checkout, please log in to check book out.</li>
+                    ): // book is not available
                     <li>This book is currently unavailable for checkout</li>
                 }
             </ul>
